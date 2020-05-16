402f0c18e2149ef6dd4873adbf6dc3f7048a3d24
TheDavtax <david.fernandezf@hotmail.com>
2020-05-17 01:31:58 +0200

    Create load_data.py

---
+++ Codes/Codes Python/load_data.py
@@ -0,0 +1,34 @@
+"""
+Scrip to load some data previously computed and manipulate it or make some plots
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Path the to file that we want to load
+folder = 'data/'
+file = 'STA_DQD_2HH_Test'
+extension = '.npy'
+file_dic = folder + file + extension
+
+data = np.load(file_dic, allow_pickle=True)  # Load the data, allow_pickle enable to load a list
+
+# Extract the data
+tau_vec = data[3]
+tf_vec = data[4]
+results = data[0]
+
+n_tf = len(tf_vec)
+n_tau = len(tau_vec)
+
+population_middle = np.zeros([n_tau, n_tf])
+fidelity = np.zeros([n_tau, n_tf])
+for i in range(0, n_tau):
+	for j in range(0, n_tf):
+		index = i * n_tf + j
+		temp = results[index]
+		population_middle[i, j] = np.max(temp[:, 2])
+		fidelity[i, j] = temp[-1, 1]
+
+fig, ax = plt.subplots()
+pos = ax.imshow(population_middle.transpose(), origin='lower', cmap='jet', aspect='auto', extent=[tau_vec[0], tau_vec[-1], tf_vec[0], tf_vec[-1]])
+cbar = fig.colorbar(pos, ax=ax)
