35d482b01f430c9ca2e569fd34b89d9ee3636dd0
TheDavtax <david.fernandezf@hotmail.com>
2020-05-18 16:00:05 +0200

    Update Test_Codes.py

--- Codes/Codes Python/Test_Codes.py
+++ Codes/Codes Python/Test_Codes.py
@@ -1,1 +1,11 @@
+import time
 
+import numpy as np
+from tqdm import tqdm
+import sys
+
+pbar = tqdm(total=100, desc='Processing', file=sys.stdout, ncols=90, bar_format='{l_bar}{bar}{r_bar}')
+
+for i in range(0, 100):
+	time.sleep(np.random.random() * 0.1)
+	pbar.update()
